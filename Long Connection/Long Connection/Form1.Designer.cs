9e1aab1154f99fa30862b1e2a15c25aa23e5efc2
hpzsr <582254172@qq.com>
2017-08-15 09:08:20 +0800

    none
    first

---
+++ Long Connection/Long Connection/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Long_Connection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_ipAddress = new System.Windows.Forms.TextBox();
+            this.textBox_ipPort = new System.Windows.Forms.TextBox();
+            this.button_start = new System.Windows.Forms.Button();
+            this.button_stop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "地址";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "端口";
+            // 
+            // textBox_ipAddress
+            // 
+            this.textBox_ipAddress.Location = new System.Drawing.Point(113, 59);
+            this.textBox_ipAddress.Name = "textBox_ipAddress";
+            this.textBox_ipAddress.Size = new System.Drawing.Size(100, 21);
+            this.textBox_ipAddress.TabIndex = 2;
+            this.textBox_ipAddress.Text = "127.0.0.1";
+            // 
+            // textBox_ipPort
+            // 
+            this.textBox_ipPort.Location = new System.Drawing.Point(113, 113);
+            this.textBox_ipPort.Name = "textBox_ipPort";
+            this.textBox_ipPort.Size = new System.Drawing.Size(100, 21);
+            this.textBox_ipPort.TabIndex = 3;
+            this.textBox_ipPort.Text = "10002";
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(58, 156);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(75, 23);
+            this.button_start.TabIndex = 4;
+            this.button_start.Text = "启动";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button_stop
+            // 
+            this.button_stop.Location = new System.Drawing.Point(170, 156);
+            this.button_stop.Name = "button_stop";
+            this.button_stop.Size = new System.Drawing.Size(75, 23);
+            this.button_stop.TabIndex = 5;
+            this.button_stop.Text = "停止";
+            this.button_stop.UseVisualStyleBackColor = true;
+            this.button_stop.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 264);
+            this.Controls.Add(this.button_stop);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.textBox_ipPort);
+            this.Controls.Add(this.textBox_ipAddress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_ipAddress;
+        private System.Windows.Forms.TextBox textBox_ipPort;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Button button_stop;
+    }
+}
+
---
+++ Long Connection/Long Connection/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Long_Connection
+{
+    public partial class Form1 : Form
+    {
+        Socket m_socket;
+        IPAddress m_ipAddress;
+        int m_ipPort;
+
+        bool m_isStart = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            m_ipAddress = IPAddress.Parse(textBox_ipAddress.Text);
+            m_ipPort = Convert.ToInt32(textBox_ipPort.Text);
+
+            //MySqlUtil.getInstance().openDatabase("game");
+
+            m_isStart = true;
+
+            Thread t1 = new Thread(new ThreadStart(ListenClient));
+            t1.Start();
+
+            ((Button)sender).Enabled = false;
+            button_stop.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            m_isStart = false;
+            m_socket.Close();
+            //MySqlUtil.getInstance().closeDatabase();
+
+            ((Button)sender).Enabled = false;
+            button_start.Enabled = true;
+        }
+
+        // 监听客户端请求
+        void ListenClient()
+        {
+            try
+            {
+                IPEndPoint iPEndPoint = new IPEndPoint(m_ipAddress, m_ipPort);
+                m_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                m_socket.Bind(iPEndPoint);
+                m_socket.Listen(400);
+
+                while (m_isStart)
+                {
+                    Socket socketback = m_socket.Accept();
+
+                    // 另开一个线程处理客户端的请求，防止阻塞
+                    Thread t1 = new Thread(new ParameterizedThreadStart(DoTaskClient));
+                    t1.Start(socketback);
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        // 处理客户端请求
+        void DoTaskClient(object socket)
+        {
+            Console.WriteLine("获取客户端发起的请求");
+
+            Socket socketback = (Socket)socket;
+
+            // 接收消息
+            receive(socketback);
+
+            //socketback.Close();
+        }
+
+        public void receive(Socket socketback)
+        {
+            byte[] rece = new byte[1024];
+            while (true)
+            {
+                if (socketback != null && socketback.Connected)
+                {
+                    int recelong = socketback.Receive(rece, rece.Length, 0);
+                    Console.WriteLine("22");
+                    string reces = Encoding.ASCII.GetString(rece, 0, recelong);
+                    // 解密
+                    //reces = _3DES.DESDecrypst(reces);
+
+                    Console.WriteLine("客户端请求数据：" + reces);
+
+                    string backData = "hello client";
+                    Console.WriteLine("返回给客户端数据：" + backData);
+
+                    // 发送消息
+                    sendmessage(socketback, backData);
+                }
+                else
+                {
+                    Console.WriteLine("连接释放");
+                }
+            }
+        }
+
+        public void sendmessage(Socket socketback, string str)
+        {
+            // 加密
+            //str = _3DES.DESEncrypt(str);
+
+            byte[] bytes = new byte[1024];
+            bytes = Encoding.ASCII.GetBytes(str);
+            socketback.Send(bytes);
+        }
+
+        void destroy()
+        {
+            m_socket.Close();
+        }
+    }
+}
