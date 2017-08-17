5b40d05733f1d36dfff22f390daeef665d6161cb
hpzsr <582254172@qq.com>
2017-08-17 17:32:39 +0800

    1

--- Long Connection/Long Connection/Form1.Designer.cs
+++ Long Connection/Long Connection/Form1.Designer.cs
@@ -33,11 +33,12 @@
             this.button_fasong = new System.Windows.Forms.Button();
             this.textBox_send = new System.Windows.Forms.TextBox();
             this.listBox_chat = new System.Windows.Forms.ListBox();
+            this.label_peopleNum = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button_start
             // 
-            this.button_start.Location = new System.Drawing.Point(63, 313);
+            this.button_start.Location = new System.Drawing.Point(376, 30);
             this.button_start.Name = "button_start";
             this.button_start.Size = new System.Drawing.Size(75, 23);
             this.button_start.TabIndex = 4;
@@ -47,7 +48,7 @@
             // 
             // button_stop
             // 
-            this.button_stop.Location = new System.Drawing.Point(160, 313);
+            this.button_stop.Location = new System.Drawing.Point(376, 73);
             this.button_stop.Name = "button_stop";
             this.button_stop.Size = new System.Drawing.Size(75, 23);
             this.button_stop.TabIndex = 5;
@@ -57,7 +58,7 @@
             // 
             // button_fasong
             // 
-            this.button_fasong.Location = new System.Drawing.Point(236, 269);
+            this.button_fasong.Location = new System.Drawing.Point(275, 313);
             this.button_fasong.Name = "button_fasong";
             this.button_fasong.Size = new System.Drawing.Size(75, 23);
             this.button_fasong.TabIndex = 6;
@@ -67,9 +68,9 @@
             // 
             // textBox_send
             // 
-            this.textBox_send.Location = new System.Drawing.Point(12, 269);
+            this.textBox_send.Location = new System.Drawing.Point(12, 313);
             this.textBox_send.Name = "textBox_send";
-            this.textBox_send.Size = new System.Drawing.Size(208, 21);
+            this.textBox_send.Size = new System.Drawing.Size(257, 21);
             this.textBox_send.TabIndex = 7;
             // 
             // listBox_chat
@@ -78,14 +79,26 @@
             this.listBox_chat.ItemHeight = 12;
             this.listBox_chat.Location = new System.Drawing.Point(12, 12);
             this.listBox_chat.Name = "listBox_chat";
-            this.listBox_chat.Size = new System.Drawing.Size(299, 220);
+            this.listBox_chat.Size = new System.Drawing.Size(338, 280);
             this.listBox_chat.TabIndex = 8;
+            // 
+            // label_peopleNum
+            // 
+            this.label_peopleNum.AutoSize = true;
+            this.label_peopleNum.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_peopleNum.Location = new System.Drawing.Point(380, 115);
+            this.label_peopleNum.Name = "label_peopleNum";
+            this.label_peopleNum.Size = new System.Drawing.Size(56, 16);
+            this.label_peopleNum.TabIndex = 9;
+            this.label_peopleNum.Text = "人数:0";
+            this.label_peopleNum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(323, 348);
+            this.ClientSize = new System.Drawing.Size(463, 348);
+            this.Controls.Add(this.label_peopleNum);
             this.Controls.Add(this.listBox_chat);
             this.Controls.Add(this.textBox_send);
             this.Controls.Add(this.button_fasong);
@@ -93,6 +106,7 @@
             this.Controls.Add(this.button_start);
             this.Name = "Form1";
             this.Text = "服务端";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,6 +118,7 @@
         private System.Windows.Forms.Button button_fasong;
         private System.Windows.Forms.TextBox textBox_send;
         private System.Windows.Forms.ListBox listBox_chat;
+        private System.Windows.Forms.Label label_peopleNum;
     }
 }
 
--- Long Connection/Long Connection/Form1.cs
+++ Long Connection/Long Connection/Form1.cs
@@ -22,9 +22,11 @@ namespace Long_Connection
         IPAddress m_ipAddress = IPAddress.Parse("127.0.0.1");
         int m_ipPort = Convert.ToInt32("10008");
 
-        Socket m_socketClient = null;
+        //Socket m_socketClient = null;
+        List<ClientSocket> m_clientSocketList = new List<ClientSocket>();
 
         bool m_isStart = false;
+        int m_clientCount = 0;
 
         public Form1()
         {
@@ -61,10 +63,16 @@ namespace Long_Connection
                 m_socket.Close();
             }
 
-            if (m_socketClient != null)
+            for (int i = 0; i < m_clientSocketList.Count; i++)
             {
-                m_socketClient.Close();
+                if (m_clientSocketList[i].m_clientSocket != null)
+                {
+                    m_clientSocketList[i].m_clientSocket.Close();
+                }
             }
+
+            m_clientSocketList.Clear();
+            this.label_peopleNum.Text = "人数:" + m_clientSocketList.Count.ToString();
 
             this.button_start.Enabled = true;
             this.button_stop.Enabled = false;
@@ -74,13 +82,23 @@ namespace Long_Connection
         // 发送消息
         private void button1_Click_1(object sender, EventArgs e)
         {
+            if (m_clientSocketList.Count <= 0)
+            {
+                Debug.WriteLine("现在没有客户端连接");
+
+                return;
+            }
+
             string backData = this.textBox_send.Text;
             this.listBox_chat.Items.Add("        " + backData);
 
             Debug.WriteLine("返回给客户端数据：" + backData);
 
             // 发送消息
-            sendmessage(m_socketClient, backData);
+            for (int i = 0; i < m_clientSocketList.Count; i++)
+            {
+                sendmessage(m_clientSocketList[i], backData);
+            }
         }
 
         // 监听客户端请求
@@ -100,13 +118,18 @@ namespace Long_Connection
                     try
                     {
                         Socket socketback = m_socket.Accept();
+
+                        ClientSocket clientSocket = new ClientSocket((++m_clientCount).ToString(), socketback);
+                        m_clientSocketList.Add(clientSocket);
+                        this.label_peopleNum.Text = "人数:" + m_clientSocketList.Count.ToString();
+
                         // 另开一个线程处理客户端的请求，防止阻塞
                         Thread t1 = new Thread(new ParameterizedThreadStart(DoTaskClient));
-                        t1.Start(socketback);
+                        t1.Start(clientSocket);
                     }
                     catch (SocketException ex)
                     {
-                        Debug.WriteLine("与客户端连接断开");
+                        Debug.WriteLine("Socket关闭");
                         Debug.WriteLine("错误日志：" + ex.Message);
                     }
                 }
@@ -118,26 +141,24 @@ namespace Long_Connection
         }
 
         // 处理客户端请求
-        void DoTaskClient(object socket)
+        void DoTaskClient(object clientSocket)
         {
             Debug.WriteLine("获取客户端发起的请求");
 
-            m_socketClient = (Socket)socket;
-
             // 接收消息
-            receive(m_socketClient);
+            receive((ClientSocket)clientSocket);
         }
 
-        public void receive(Socket socketback)
+        public void receive(ClientSocket clientSocket)
         {
             byte[] rece = new byte[1024];
             while (m_isStart)
             {
-                if (socketback != null && socketback.Connected)
+                if (clientSocket.m_clientSocket != null && clientSocket.m_clientSocket.Connected)
                 {
                     try
                     {
-                        int recelong = socketback.Receive(rece, rece.Length, 0);
+                        int recelong = clientSocket.m_clientSocket.Receive(rece, rece.Length, 0);
                         string reces = Encoding.UTF8.GetString(rece, 0, recelong);
                         // 解密
                         //reces = _3DES.DESDecrypst(reces);
@@ -149,7 +170,10 @@ namespace Long_Connection
                     }
                     catch (SocketException ex)
                     {
-                        Debug.WriteLine("与客户端连接断开");
+                        m_clientSocketList.Remove(clientSocket);
+                        this.label_peopleNum.Text = "人数:" + m_clientSocketList.Count.ToString();
+
+                        Debug.WriteLine("与客户端连接断开:"+ clientSocket.m_id);
                         Debug.WriteLine("错误日志：" + ex.Message);
                         return;
                     }
@@ -161,18 +185,18 @@ namespace Long_Connection
             }
         }
 
-        public void sendmessage(Socket socketback, string str)
+        public void sendmessage(ClientSocket clientSocket, string str)
         {
             // 加密
             //str = _3DES.DESEncrypt(str);
 
             try
             {
-                if (socketback != null)
+                if (clientSocket.m_clientSocket != null)
                 {
                     byte[] bytes = new byte[1024];
                     bytes = Encoding.UTF8.GetBytes(str);
-                    socketback.Send(bytes);
+                    clientSocket.m_clientSocket.Send(bytes);
                 }
                 else
                 {
@@ -181,14 +205,50 @@ namespace Long_Connection
             }
             catch (SocketException ex)
             {
-                Debug.WriteLine("与客户端连接断开");
+                m_clientSocketList.Remove(clientSocket);
+                this.label_peopleNum.Text = "人数:" + m_clientSocketList.Count.ToString();
+
+                Debug.WriteLine("与客户端连接断开:" + clientSocket.m_id);
                 Debug.WriteLine("错误日志：" + ex.Message);
             }
         }
 
         void destroy()
         {
-            m_socket.Close();
+            Debug.WriteLine("服务端关闭");
+
+            m_isStart = false;
+
+            if (m_socket != null)
+            {
+                m_socket.Close();
+            }
+
+            for (int i = 0; i < m_clientSocketList.Count; i++)
+            {
+                if (m_clientSocketList[i].m_clientSocket != null)
+                {
+                    m_clientSocketList[i].m_clientSocket.Close();
+                }
+            }
+        }
+
+        // 关闭窗口
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            destroy();
         }
     }
 }
+
+public class ClientSocket
+{
+    public string m_id = "";
+    public Socket m_clientSocket = null;
+
+    public ClientSocket(string id, Socket clientSocket)
+    {
+        m_id = id;
+        m_clientSocket = clientSocket;
+    }
+};
