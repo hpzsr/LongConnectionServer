330ed8dce392b54b2fbc9b56ebdd8d8f94d85ac5
hpzsr <582254172@qq.com>
2017-08-21 00:41:18 +0800

    增加几个接口

--- Long Connection/Long Connection/Form1.cs
+++ Long Connection/Long Connection/Form1.cs
@@ -21,7 +21,7 @@ namespace Long_Connection
         //IPAddress m_ipAddress = IPAddress.Parse("222.73.65.206");
         IPAddress m_ipAddress = IPAddress.Parse("127.0.0.1");
         int m_ipPort = Convert.ToInt32("10103");
-        
+
         List<ClientData> m_clientDataList = new List<ClientData>();
 
         bool m_isStart = false;
@@ -90,6 +90,7 @@ namespace Long_Connection
             }
 
             string backData = this.textBox_send.Text;
+            this.textBox_send.Text = "";
             this.listBox_chat.Items.Add("        " + backData);
 
             Debug.WriteLine("返回给客户端数据：" + backData);
@@ -119,39 +120,13 @@ namespace Long_Connection
                     {
                         Socket socketback = m_socket.Accept();
 
-                        ClientData clientData = new ClientData((++m_clientCount).ToString(), socketback);
-                        if (m_clientDataList.Count == 1)
-                        {
-                            clientData.m_data = ((int)Consts_Code.HeroAction.HeroAction_Idle).ToString() + ";90";
-                        }
-                        else if (m_clientDataList.Count == 2)
-                        {
-                            clientData.m_data = ((int)Consts_Code.HeroAction.HeroAction_Idle).ToString() + ";-90";
-                        }
+                        ClientData clientData = new ClientData(socketback);
                         m_clientDataList.Add(clientData);
                         this.label_peopleNum.Text = "人数:" + m_clientDataList.Count.ToString();
-
-                        // 发送玩家的初始状态信息：位置、角度
-                        {
-                            JObject jo = new JObject();
-                            jo.Add("tag", "InitGame");
-                            jo.Add("data", clientData.m_data);
-                            sendmessage(clientData, jo.ToString());
-                        }
-
+                        
                         // 另开一个线程处理客户端的请求，防止阻塞
                         Thread t1 = new Thread(new ParameterizedThreadStart(DoTaskClient));
                         t1.Start(clientData);
-
-
-                        // 检测人是否已经齐了
-                        {
-                            if (m_clientDataList.Count == 2)
-                            {
-                                Thread t2 = new Thread(peopleAllIn);
-                                t2.Start();
-                            }
-                        }
                     }
                     catch (SocketException ex)
                     {
@@ -175,31 +150,6 @@ namespace Long_Connection
             receive((ClientData)clientData);
         }
 
-        void peopleAllIn()
-        {
-            m_timer = new System.Threading.Timer(callClient, "", 1000, 1000 / 30);
-        }
-
-        // 主动联系客户端
-        void callClient(object data)
-        {
-            JArray ja = new JArray();
-
-            for (int i = 0; i < m_clientDataList.Count; i++)
-            {
-                JObject jo = new JObject();
-                jo.Add("id", m_clientDataList[i].m_id);
-                jo.Add("data", m_clientDataList[i].m_data);
-                ja.Add(jo);
-            }
-
-            // 发送消息
-            for (int i = 0; i < m_clientDataList.Count; i++)
-            {
-                sendmessage(m_clientDataList[i], ja.ToString());
-            }
-        }
-
         public void receive(ClientData clientData)
         {
             byte[] rece = new byte[1024];
@@ -217,13 +167,44 @@ namespace Long_Connection
                         Debug.WriteLine("客户端请求数据：" + reces);
 
                         this.listBox_chat.Items.Add(reces);
+
+                        string tag = "";
+                        try
+                        {
+                            JObject jo = JObject.Parse(reces);
+                            tag = jo.GetValue("tag").ToString();
+                        }
+                        catch (Exception ex)
+                        {
+                            Debug.WriteLine("json格式不对");
+                        }
+
+                        // 客户端告诉服务端自己的ID
+                        if (tag.CompareTo("") == 0)
+                        {
+                            // 是否要告诉客户端传的json不对?
+                        }
+                        else if (tag.CompareTo("TellSelfId") == 0)
+                        {
+                            onReq_TellSelfId(clientData, reces);
+                        }
+                        // 客户端告诉服务端自己已经初始化场景完成
+                        else if (tag.CompareTo("TellSelfIsInited") == 0)
+                        {
+                            onReq_TellSelfIsInited(clientData);
+                        }
+                        // 客户端告诉服务端自己的状态信息：动作、角度
+                        else if (tag.CompareTo("TellSelfStateInfo") == 0)
+                        {
+                            onReq_TellSelfStateInfo(clientData, reces);
+                        }
                     }
                     catch (SocketException ex)
                     {
                         m_clientDataList.Remove(clientData);
                         this.label_peopleNum.Text = "人数:" + m_clientDataList.Count.ToString();
 
-                        Debug.WriteLine("与客户端连接断开:"+ clientData.m_id);
+                        Debug.WriteLine("与客户端连接断开:" + clientData.m_id);
                         Debug.WriteLine("错误日志：" + ex.Message);
                         return;
                     }
@@ -288,6 +269,148 @@ namespace Long_Connection
         {
             destroy();
         }
+
+        //--------------------------------处理客户端请求 start-----------------------
+
+        // 客户端告诉服务端自己的ID
+        void onReq_TellSelfId(ClientData clientData, string jsonData)
+        {
+            // 取到客户端传来的id
+            {
+                JObject jo = JObject.Parse(jsonData);
+                string id = jo.GetValue("uid").ToString();
+
+                clientData.m_id = id;
+            }
+
+            // 只有进入两个人才可开始游戏
+            {
+                if (m_clientDataList.Count != 2)
+                {
+                    return;
+                }
+            }
+
+            // 检测是否所有人都提交了自己的id信息
+            {
+                for (int i = 0; i < m_clientDataList.Count; i++)
+                {
+                    if (m_clientDataList[i].m_id .CompareTo("") == 0)
+                    {
+                        return;
+                    }
+                }
+            }
+
+            //--------------------------------------执行到这里说明所有人都提交了
+
+            // 设置每个人的初始状态和位置
+            {
+                for (int i = 0; i < m_clientDataList.Count; i++)
+                {
+                    if ((i % 2) == 0)
+                    {
+                        m_clientDataList[i].m_data = ((int)Consts_Code.HeroAction.HeroAction_Idle).ToString() + ";90";
+                    }
+                    else
+                    {
+                        m_clientDataList[i].m_data = ((int)Consts_Code.HeroAction.HeroAction_Idle).ToString() + ";-90";
+                    }
+                }
+            }
+
+            // 推送自己和别人的初始状态和位置
+            {
+                JObject jo = new JObject();
+                jo.Add("tag","InitStateInfo");
+
+                {
+                    JArray ja = new JArray();
+
+                    for (int i = 0; i < m_clientDataList.Count; i++)
+                    {
+                        JObject jo2 = new JObject();
+                        jo2.Add("id", m_clientDataList[i].m_id);
+                        jo2.Add("data", m_clientDataList[i].m_data);
+                        ja.Add(jo2);
+                    }
+
+                    jo.Add("stateInfo", ja);
+                }
+                // 发送消息
+                for (int i = 0; i < m_clientDataList.Count; i++)
+                {
+                    sendmessage(m_clientDataList[i], jo.ToString());
+                }
+            }
+        }
+
+        // 客户端告诉服务端自己已经初始化场景完成
+        void onReq_TellSelfIsInited(ClientData clientData)
+        {
+            clientData.m_isInited = 1;
+
+            // 检测所有人是否都初始化场景完成
+            {
+                for (int i = 0; i < m_clientDataList.Count; i++)
+                {
+                    if (m_clientDataList[i].m_isInited == 0)
+                    {
+                        return;
+                    }
+                }
+            }
+
+            // ------------------------------------执行到这里说明所有人都初始化场景完成
+
+            // 开始定时广播所有人的状态信息
+            {
+                Thread t = new Thread(SendAllPeopleStateInfo);
+                t.Start();
+            }
+        }
+
+        // 客户端告诉服务端自己的状态信息：动作、角度
+        void onReq_TellSelfStateInfo(ClientData clientData, string jsonData)
+        {
+            JObject jo = JObject.Parse(jsonData);
+            string data = jo.GetValue("data").ToString();
+
+            clientData.m_data = data;
+        }
+
+        //--------------------------------处理客户端请求 end-------------------------
+
+        void SendAllPeopleStateInfo()
+        {
+            m_timer = new System.Threading.Timer(callClient, "", 1000, 1000 / 30);
+        }
+
+        // 主动联系客户端
+        void callClient(object data)
+        {
+            JObject jo = new JObject();
+            jo.Add("tag", "SynAllStateInfo");
+
+            {
+                JArray ja = new JArray();
+
+                for (int i = 0; i < m_clientDataList.Count; i++)
+                {
+                    JObject jo2 = new JObject();
+                    jo2.Add("id", m_clientDataList[i].m_id);
+                    jo2.Add("data", m_clientDataList[i].m_data);
+                    ja.Add(jo2);
+                }
+
+                jo.Add("stateInfo", ja);
+            }
+            // 发送消息
+            for (int i = 0; i < m_clientDataList.Count; i++)
+            {
+                sendmessage(m_clientDataList[i], jo.ToString());
+            }
+        }
     }
 }
 
@@ -295,11 +418,11 @@ public class ClientData
 {
     public string m_id = "";
     public string m_data = "";
+    public int m_isInited = 0;
     public Socket m_clientData = null;
     
-    public ClientData(string id, Socket clientData)
+    public ClientData(Socket clientData)
     {
-        m_id = id;
         m_clientData = clientData;
     }
 };
